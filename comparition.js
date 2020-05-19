c7b921def27f21a4f1e7419ae2e1a7d8f8f9c79f
Leonardo Lucas <leonardo.lucas@unesp.br>
2020-05-19 13:58:39 -0300

    Build 0.5
    Allergy -benefit range - logic errors - html design

--- comparition.js
+++ comparition.js
@@ -20,13 +20,14 @@ var productRef = firebase.database().ref('products');
 var name_pro = [];
 var knowas = [];
 var origin_pro = [];
-var score = [];
 var main_out = [];
 var sec_out = [];
 var thi_out = [];
 var fou_out = [];
 var price = [];
 var aux = [];
+var source= [];
+var sourceR = [];;
   
  productRef.on('value', gotData, errData);
  function gotData(data) {
@@ -42,12 +43,16 @@ var aux = [];
      thi_out[i] = products[k].thi_out;
      fou_out[i] = products[k].fou_out;
      price[i] = products[k].price;
-     score[i] = 0;
+     source[i] = products[k].source;
      var name1 = name_pro[i];
      var knowas1 = knowas[i];
      var price1 = price[i];
-     var score1 = score[i];
-     aux.push({score1,name1,knowas1,price1});
+     var score1 = 0;
+     var sourceA = [];
+     sourceA = source[i].split(", ");
+     sourceR.push(sourceA);
+     var outcome1 = '';
+     aux.push({score1,name1,knowas1,price1,outcome1});
   }
 
 }
@@ -58,16 +63,16 @@ function errData(err) {
 
   
   // Listen for form submit
-  document.getElementById('quiz_form').addEventListener('submit', getResult);
-  
-
-function getResult(e){
-  var results = '';
+  $('form').submit(function(e){
   e.preventDefault();
+  var data_aux = aux.map(Element=>{
+    return Element;
+  });
+  var results = '';
   var data = {};
   var dataArray = $("form").serializeArray();
   for(var i=0;i<dataArray.length;i++){
-    if (dataArray[i].name == "client_benefits" || dataArray[i].name == "client_allergy") {
+    if (dataArray[i].name == "client_benefits" || dataArray[i].name == "client_allergy" || dataArray[i].name == "rclient_benefits") {
      if (!data[dataArray[i].name]){
       data[dataArray[i].name] = []
      }
@@ -76,54 +81,88 @@ function getResult(e){
       data[dataArray[i].name] = dataArray[i].value;
   }
 }
-for (var i=0; i<main_out.length-1 || i<sec_out.length-1 || i<thi_out.length-1 || i<fou_out.length-1;i++){
-  //score[i] = 0;
-for(var j=0; j <= data.client_benefits.length-1 ;j++){
 
+//source
+  //asource = sourceR[n];
+  if (data.client_allergy != null){
+  for(var p=0;p<sourceR.length;p++){
+    var asource = sourceR[p];
+    for(var x=0;x<asource.length;x++){
+   for(var o=0; o <= data.client_allergy.length-1 ;o++){ 
+    if(asource[x] == data.client_allergy[o]){
+    data_aux[p].score1 = -1;
+   }//for client allergy
+  }//for asource
+}
+}
+  }
+
+if (data.client_benefits != null){
+for (var i=0; i<main_out.length || i<sec_out.length || i<thi_out.length || i<fou_out.length;i++){
+  if(data_aux[i].score1 != -1){
+  data_aux[i].score1 = 0;
+  data_aux[i].outcome1 = '';
+for(var j=0; j <= data.client_benefits.length ;j++){
   if(main_out[i] == data.client_benefits[j]){
-    aux[i].score1 += 10;
+    data_aux[i].score1 += 10 + (data.rclient_benefits[j]*2);
+    data_aux[i].outcome1 = main_out[i] + ' | ' + data_aux[i].outcome1;
   }
   if(sec_out[i] == data.client_benefits[j]){
-    aux[i].score1 += 8;
+    data_aux[i].score1 += 8 + (data.rclient_benefits[j]*2);
+    data_aux[i].outcome1 = sec_out[i] + ' | ' + data_aux[i].outcome1;
   }
        if(thi_out[i] == data.client_benefits[j]){
-    aux[i].score1 += 6;
+    data_aux[i].score1 += 6 + (data.rclient_benefits[j]*2);
+    data_aux[i].outcome1 = thi_out[i] + ' | ' + data_aux[i].outcome1;
   }
        if(fou_out[i] == data.client_benefits[j]){
-    aux[i].score1 += 4;
+    data_aux[i].score1 += 4 + (data.rclient_benefits[j]*2);
+    data_aux[i].outcome1 = fou_out[i] + ' | ' + data_aux[i].outcome1;
   }     
 }
-
-
+  }
 } 
+console.log(data_aux);
+data_aux.sort(function(current, next){return next.score1 - current.score1});
 
-aux.sort(function(current, next){return next.score1 - current.score1});
-
-//console.log(aux.knowas);
-//quickSort(score,0 ,score.length -1)
-//aux.sort((a, b) => b - a);
-//var aa;
-//var novo = map(value => ({'key':value, 'val': aa = knowas.map() }));
-console.log(aux);
-//console.log(novo);
-document.querySelector('.tableh').style.display = 'block';
-
-for(var i=0;i<aux.length;i++){
-  if(aux[0].score1 - 10 <= aux[i].score1){
+for(var i=0;i<data_aux.length;i++){
+  if(data_aux[0].score1 - 6 <= data_aux[i].score1){
 //Table Generator
     results = results+`<tr>
     <th scope="row">`+(i+1)+`</th>
-    <td>`+aux[i].name1+`</td>
-    <td>`+aux[i].knowas1+`</td>
-    <td>`+aux[i].price1+`</td>
+    <td>`+data_aux[i].name1+`</td>
+    <td>`+data_aux[i].knowas1+`</td>
+    <td>`+data_aux[i].price1+`</td>
+    <td>`+data_aux[i].outcome1+`</td>
     </tr>`}
 }
 
 document.getElementById('result_row').innerHTML = results;
+document.querySelector('.tableh').style.display = 'block';
+} 
+  }
+  )
+$('.custom-control-input').change(function() {
+  if ($(this).prop('checked')) {
+    $(this).parent().find('.custom-range').prop('disabled', false);
+    $(this).parent().find('.rangeh').show();
+  }
+  else{
+    $(this).parent().find('.custom-range').prop('disabled', true);
+    $(this).parent().find('.rangeh').hide();
+  }
 }
+)
 
 
+// function rangeshowup(checkboxElem) {
+// if (checkboxElem.checked == true){
+// //document.querySelector('.tableh').style.display = 'block';
+// $(checkboxElem).parent().find('rangeh').style.display = 'block';
 
-
-  
-
+// }
+// else {
+//   //document.querySelector('.rangeh').style.display = 'none';
+//   $(checkboxElem).parent().find('rangeh').style.display = 'none';
+// }
+// }
